774f69c3b6e94ec9f48567dab60c37552aae3c9f
Konrad Baran <konradbaran95@gmail.com>
2025-11-16 10:28:09 +0100

    remove secrets from code

--- tests/test_first_trello_board.py
+++ tests/test_first_trello_board.py
@@ -1,20 +1,22 @@
 import requests
 import pytest
 
-API_KEY = "18a796421b243fda27700d4e49bbccde"
 
-TOKEN = "ATTAf82e8f88f07b695c04c41009be80d41ccf80a8080cb60844eb6842158b4e054bC85AF46E"
+# response = requests.get(f"{BASE_URL}/{ENDPOINT}").json()
+# # r = response.json()
+# # print(r)
+# print(response)
 
-BASE_URL = "https://api.trello.com/1/"
 
+# def test_create_and_delete_board():
 
+endpoint = "boards/"
+board_name = "python test board 1"
+query_params_post = {
+    "name": board_name,
+    
+}
 
-response = requests.get(f"{BASE_URL}/{ENDPOINT}").json()
-# r = response.json()
-# print(r)
-print(response)
-
-def test_create_and_delete_board():
-
-    board_name = "python test board 1"
-    pass
+create_board_response = requests.post(f"{BASE_URL}/{endpoint}")
+print(create_board_response.status_code)
+    
