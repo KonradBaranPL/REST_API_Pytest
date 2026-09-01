0c834b9ee59aea5e6ea0976a2a7209e7922886cb
Konrad Baran <konradbaran95@gmail.com>
2026-09-01 20:45:34 +0200

    feat (api clients): add basic methods definitions

--- api_clients/cards_client.py
+++ api_clients/cards_client.py
@@ -10,3 +10,33 @@ class CardClient(BaseClient):
         """Initialize CardsClient and set the endpoint to 'boards/'."""
         super().__init__()
         self.endpoint = "cards/"
+
+
+    def create_card(self):
+        """Create a new card on a list"""
+        pass
+
+    def get_card(self):
+        """Get details about a specific single card"""
+        pass
+
+    def update_card(self):
+        pass
+
+    def delete_card(self):
+        pass
+
+    def get_list(self):
+        pass
+
+    def add_comment(self):
+        pass
+
+    def delete_comment(self):
+        pass
+
+    def create_label(self):
+        pass
+
+    def remove_label(self):
+        pass
